4a58c6368401835943f33900eaa211e020514b5f
faavalos94 <faavalos94@gmail.com>
2025-08-11 21:40:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,5 +31,5 @@ az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-ins
 
 # Create data assets
 echo "Create training data asset:"
-az ml data create --type uri_file --name "tickets-data" --path ./Data/it-ticks.csv
+az ml data create --type uri_file --name "tickets-data" --path ./Data/it_ticks.csv
 
